889d142c999cb270ad57426a242369f86b34d320
Vaxjo <matiz12345678901@wp.pl>
2023-03-14 12:18:46 +0100

    finish lab4

--- Lab4V2/html-removing-application/src/main/java/com/example/Application.java
+++ Lab4V2/html-removing-application/src/main/java/com/example/Application.java
@@ -4,8 +4,12 @@ package com.example;
 
 public class Application {
     public static void main(String[] args) {
+        String url = args[0];
+        PageDownloadingUtility downloader = new PageDownloadingUtility();
+        // String textWithHtml = "<h1>Hello, <i>world</i>!</h1>";
+        String textWithHtml = downloader.download(url);
+        
         StringHtmlCleaner remover = new StringHtmlCleaner();
-        String textWithHtml = "<h1>Hello, <i>world</i>!</h1>";
         String textWithoutHtml = remover.cleanText(textWithHtml);
         System.out.println(textWithoutHtml);
     }
