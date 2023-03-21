e3b5950975e07377a42e71644910f84192ff37b8
Vaxjo <matiz12345678901@wp.pl>
2023-03-21 22:35:09 +0100

    Lab5 zad1

--- Lab4V4/html-removing-application/src/main/java/com/example/Application.java
+++ Lab4V4/html-removing-application/src/main/java/com/example/Application.java
@@ -6,7 +6,6 @@ public class Application {
     public static void main(String[] args) {
         String url = args[0];
         PageDownloadingUtility downloader = new PageDownloadingUtility();
-        // String textWithHtml = "<h1>Hello, <i>world</i>!</h1>";
         String textWithHtml = downloader.download(url);
         
         StringHtmlCleaner remover = new StringHtmlCleaner();
--- Lab5/Lab5/src/FeaturesCalculator.java
+++ Lab5/Lab5/src/FeaturesCalculator.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,8 +12,9 @@ public class FeaturesCalculator {
 
     public FeaturesCalculator(String filename) {
         this.filename = filename;
-        read_data(filename);
+        read_data(this.filename);
         calculate_mass_center();
+        save_results();
     }
 
     public void read_data(String currentFilename) {
@@ -19,20 +22,20 @@ public class FeaturesCalculator {
             File data = new File(currentFilename);
             Scanner fileScanner = new Scanner(data);
             binary_mask = new ArrayList<ArrayList<Integer>>();
-            while (fileScanner.hasNextLine()) {
-                ArrayList<Integer> row = new ArrayList<>();
+            while (fileScanner.hasNextLine()) { //do ostatniej linii
+                ArrayList<Integer> row = new ArrayList<>(); //wiersz jako arraylista
                 String line = fileScanner.nextLine();
                 String[] lineArray = line.split(" ");
                 for (String value : lineArray) {
                     row.add(Integer.parseInt(value));
                 }
                 binary_mask.add(row);
-                System.out.println(line);
+                // System.out.println(line);
             }
-            System.out.println("\n" + binary_mask);
+            // System.out.println("\n" + binary_mask);
             fileScanner.close();
 
-        } catch (FileNotFoundException err) {
+        } catch (FileNotFoundException err) { //jezeli nie znajdzie pliku
             System.out.println(currentFilename + " not found");
         }
     }
@@ -49,7 +52,7 @@ public class FeaturesCalculator {
             for (Integer integer : arrayList) {
                 x++;
                 if (integer == 1) {
-                    System.out.print("(" + x + ", " + y + ")");
+                    // System.out.print("(" + x + ", " + y + ")");
                     n++;
                     this.mass_center[0] = (x + this.mass_center[0]);
                     this.mass_center[1] = (y + this.mass_center[1]);
@@ -57,24 +60,55 @@ public class FeaturesCalculator {
             }
             x = 0;
         }
-        System.out.println("");
+        // System.out.println("");
         this.mass_center[0] = this.mass_center[0] / n;
         this.mass_center[1] = this.mass_center[1] / n;
-        System.out.println("mass center: (" + this.mass_center[0] + ", " + this.mass_center[1] + ")");
+        // System.out.println("mass center: (" + this.mass_center[0] + ", " + this.mass_center[1] + ")");
     }
 
     public void save_results() {
+        try {
+            File resultFile = new File(filename.replace(".", "_out."));
+            FileWriter writer = new FileWriter(resultFile);
+            writer.write("P = (" + this.mass_center[0] + ", " + this.mass_center[1] + ")");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Boolean filenameCheck(String currentFilename) {
+        String[] words = currentFilename.split("\\.");
+        String[] firstSegment;
+        if (words.length != 2) {
+            return false;
+        } else {
+            firstSegment = words[0].split("_");
+        }
+
+        if (!words[1].equals("txt")) {
+            return false;
+        } else if (firstSegment.length != 3) {
+            return false;
+        } else if (!firstSegment[0].equals("binary") || !firstSegment[1].equals("mask")) {
+            return false;
+        } else if (Integer.parseInt(firstSegment[2]) > 3 || Integer.parseInt(firstSegment[2]) < 0) {
+            return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        // Scanner scanner = new Scanner(System.in);
-        // System.out.print("Enter a filename: ");
-        // String value = scanner.nextLine();
-        String value = "binary_mask_2.txt";
-        FeaturesCalculator obj = new FeaturesCalculator(value);
-        // scanner.close();
-
-        // obj.read_data("binary_mask_1.txt");
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a filename: ");
+        String value = scanner.nextLine();
+        // String value = "binary_mask_3.txt";
+        Boolean check = filenameCheck(value);
+        if (check == true) {
+            FeaturesCalculator obj = new FeaturesCalculator(value);
+        } else {
+            System.out.println("zla nazwa pliku");
+        }
+        scanner.close();
     }
 }
